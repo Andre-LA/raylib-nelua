991107b7fc21b65ef5304d7aceab3439ebd2c25c
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2020-05-04 17:39:56 -0300

    move color defines to end on modified-raylib.h

--- binding_generator/modified-raylib.h
+++ binding_generator/modified-raylib.h
@@ -129,37 +129,6 @@
 #else
     #define CLITERAL(type)      (type)
 #endif
-*/
-
-// Some Basic Colors
-// NOTE: Custom raylib color palette for amazing visuals on WHITE background
-#define LIGHTGRAY  /*CLITERAL(Color)*/{ 200, 200, 200, 255 }   // Light Gray
-#define GRAY       /*CLITERAL(Color)*/{ 130, 130, 130, 255 }   // Gray
-#define DARKGRAY   /*CLITERAL(Color)*/{ 80, 80, 80, 255 }      // Dark Gray
-#define YELLOW     /*CLITERAL(Color)*/{ 253, 249, 0, 255 }     // Yellow
-#define GOLD       /*CLITERAL(Color)*/{ 255, 203, 0, 255 }     // Gold
-#define ORANGE     /*CLITERAL(Color)*/{ 255, 161, 0, 255 }     // Orange
-#define PINK       /*CLITERAL(Color)*/{ 255, 109, 194, 255 }   // Pink
-#define RED        /*CLITERAL(Color)*/{ 230, 41, 55, 255 }     // Red
-#define MAROON     /*CLITERAL(Color)*/{ 190, 33, 55, 255 }     // Maroon
-#define GREEN      /*CLITERAL(Color)*/{ 0, 228, 48, 255 }      // Green
-#define LIME       /*CLITERAL(Color)*/{ 0, 158, 47, 255 }      // Lime
-#define DARKGREEN  /*CLITERAL(Color)*/{ 0, 117, 44, 255 }      // Dark Green
-#define SKYBLUE    /*CLITERAL(Color)*/{ 102, 191, 255, 255 }   // Sky Blue
-#define BLUE       /*CLITERAL(Color)*/{ 0, 121, 241, 255 }     // Blue
-#define DARKBLUE   /*CLITERAL(Color)*/{ 0, 82, 172, 255 }      // Dark Blue
-#define PURPLE     /*CLITERAL(Color)*/{ 200, 122, 255, 255 }   // Purple
-#define VIOLET     /*CLITERAL(Color)*/{ 135, 60, 190, 255 }    // Violet
-#define DARKPURPLE /*CLITERAL(Color)*/{ 112, 31, 126, 255 }    // Dark Purple
-#define BEIGE      /*CLITERAL(Color)*/{ 211, 176, 131, 255 }   // Beige
-#define BROWN      /*CLITERAL(Color)*/{ 127, 106, 79, 255 }    // Brown
-#define DARKBROWN  /*CLITERAL(Color)*/{ 76, 63, 47, 255 }      // Dark Brown
-
-#define WHITE      /*CLITERAL(Color)*/{ 255, 255, 255, 255 }   // White
-#define BLACK      /*CLITERAL(Color)*/{ 0, 0, 0, 255 }         // Black
-#define BLANK      /*CLITERAL(Color)*/{ 0, 0, 0, 0 }           // Blank (Transparent)
-#define MAGENTA    /*CLITERAL(Color)*/{ 255, 0, 255, 255 }     // Magenta
-#define RAYWHITE   /*CLITERAL(Color)*/{ 245, 245, 245, 255 }   // My own White (raylib logo)
 
 // Temporal hack to avoid breaking old codebases using
 // deprecated raylib implementation of these functions
@@ -167,6 +136,7 @@
 #define SubText     TextSubtext
 #define ShowWindow  UnhideWindow
 #define LoadText    LoadFileText
+*/
 
 //----------------------------------------------------------------------------------
 // Structures Definition
@@ -1479,3 +1449,34 @@ extern "C" {            // Prevents name mangling of functions
 
 #endif // RAYLIB_H
 */
+
+// (raylib-nelua modification, colors defines moved to end)
+// Some Basic Colors
+// NOTE: Custom raylib color palette for amazing visuals on WHITE background
+#define LIGHTGRAY  (Color){ 200, 200, 200, 255 }   // Light Gray
+#define GRAY       (Color){ 130, 130, 130, 255 }   // Gray
+#define DARKGRAY   (Color){ 80, 80, 80, 255 }      // Dark Gray
+#define YELLOW     (Color){ 253, 249, 0, 255 }     // Yellow
+#define GOLD       (Color){ 255, 203, 0, 255 }     // Gold
+#define ORANGE     (Color){ 255, 161, 0, 255 }     // Orange
+#define PINK       (Color){ 255, 109, 194, 255 }   // Pink
+#define RED        (Color){ 230, 41, 55, 255 }     // Red
+#define MAROON     (Color){ 190, 33, 55, 255 }     // Maroon
+#define GREEN      (Color){ 0, 228, 48, 255 }      // Green
+#define LIME       (Color){ 0, 158, 47, 255 }      // Lime
+#define DARKGREEN  (Color){ 0, 117, 44, 255 }      // Dark Green
+#define SKYBLUE    (Color){ 102, 191, 255, 255 }   // Sky Blue
+#define BLUE       (Color){ 0, 121, 241, 255 }     // Blue
+#define DARKBLUE   (Color){ 0, 82, 172, 255 }      // Dark Blue
+#define PURPLE     (Color){ 200, 122, 255, 255 }   // Purple
+#define VIOLET     (Color){ 135, 60, 190, 255 }    // Violet
+#define DARKPURPLE (Color){ 112, 31, 126, 255 }    // Dark Purple
+#define BEIGE      (Color){ 211, 176, 131, 255 }   // Beige
+#define BROWN      (Color){ 127, 106, 79, 255 }    // Brown
+#define DARKBROWN  (Color){ 76, 63, 47, 255 }      // Dark Brown
+
+#define WHITE      (Color){ 255, 255, 255, 255 }   // White
+#define BLACK      (Color){ 0, 0, 0, 255 }         // Black
+#define BLANK      (Color){ 0, 0, 0, 0 }           // Blank (Transparent)
+#define MAGENTA    (Color){ 255, 0, 255, 255 }     // Magenta
+#define RAYWHITE   (Color){ 245, 245, 245, 255 }   // My own White (raylib logo)
