f81a17666b105baf38031a4ccb0ba10842566098
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2020-09-16 16:55:52 -0300

    add unbounded array types on function returns and arguments

--- binding_generator/raylib-binding-generator.lua
+++ binding_generator/raylib-binding-generator.lua
@@ -56,48 +56,138 @@ end
 
 table.insert(final_result, '\n')
 
+-- creates a code pattern
+-- e.g.:  "Raylib.DecompressData(compData: *cuchar" -> "Raylib%.DecompressData%(compData: %*cuchar"
+local function codepatt(code)
+  return code:gsub('%%', '%%%%')
+             :gsub('%.', '%%.')
+             :gsub('%*', '%%*')
+             :gsub('%(', '%%(')
+             :gsub('%)', '%%)')
+             :gsub('%[', '%%[')
+             :gsub('%]', '%%]')
+end
+-- this modifies final_result[i]!
+local function apply_unbounded_array(i, originalcode, replcode)
+  local patt = codepatt(originalcode)
+  local fr_i, n = string.gsub(final_result[i], patt, replcode)
+  assert(n > 0, string.format('apply_unbounded_array: nothing was applied: codepatt: %s; replcode: %s', patt, replcode))
+  final_result[i] = fr_i
+end
+
 for i = 1, #final_result do
-  if string.find(final_result[i], 'global Font ') then
-    final_result[i] = string.gsub(final_result[i], 'recs: %*Rectangle', 'recs: *[0]Rectangle')
-    final_result[i] = string.gsub(final_result[i], 'chars: %*CharInfo', 'recs: *[0]CharInfo')
+  local find = string.find
+
+  -- apply undbounded array type to records
+  if find(final_result[i], 'global Font ', 1, true) then
+    apply_unbounded_array(i, 'recs: *Rectangle', 'recs: *[0]Rectangle')
+    apply_unbounded_array(i, 'chars: *CharInfo', 'chars: *[0]CharInfo')
   end
 
-  if string.find(final_result[i], 'global Mesh ') then
-    final_result[i] = string.gsub(final_result[i], 'vertices: %*float32', 'vertices: *[0]float32')
-    final_result[i] = string.gsub(final_result[i], 'texcoords: %*float32', 'texcoords: *[0]float32')
-    final_result[i] = string.gsub(final_result[i], 'texcoords2: %*float32', 'texcoords2: *[0]float32')
-    final_result[i] = string.gsub(final_result[i], 'normals: %*float32', 'normals: *[0]float32')
-    final_result[i] = string.gsub(final_result[i], 'tangents: %*float32', 'tangents: *[0]float32')
-    final_result[i] = string.gsub(final_result[i], 'colors: %*cuchar', 'colors: *[0]cuchar')
-    final_result[i] = string.gsub(final_result[i], 'indices: %*cushort', 'indices: *[0]cushort')
-    final_result[i] = string.gsub(final_result[i], 'animVertices: %*float32', 'animVertices: *[0]float32')
-    final_result[i] = string.gsub(final_result[i], 'animNormals: %*float32', 'animNormals: *[0]float32')
-    final_result[i] = string.gsub(final_result[i], 'boneIds: %*cint', 'boneIds: *[0]cint')
-    final_result[i] = string.gsub(final_result[i], 'boneWeights: %*float32', 'boneWeights: *[0]float32')
-    final_result[i] = string.gsub(final_result[i], 'vboId: %*cuint', 'vboId: *[0]cuint')
+  if find(final_result[i], 'global Mesh ', 1, true) then
+    apply_unbounded_array(i, 'vertices: *float32', 'vertices: *[0]float32')
+    apply_unbounded_array(i, 'texcoords: *float32', 'texcoords: *[0]float32')
+    apply_unbounded_array(i, 'texcoords2: *float32', 'texcoords2: *[0]float32')
+    apply_unbounded_array(i, 'normals: *float32', 'normals: *[0]float32')
+    apply_unbounded_array(i, 'tangents: *float32', 'tangents: *[0]float32')
+    apply_unbounded_array(i, 'colors: *cuchar', 'colors: *[0]cuchar')
+    apply_unbounded_array(i, 'indices: *cushort', 'indices: *[0]cushort')
+    apply_unbounded_array(i, 'animVertices: *float32', 'animVertices: *[0]float32')
+    apply_unbounded_array(i, 'animNormals: *float32', 'animNormals: *[0]float32')
+    apply_unbounded_array(i, 'boneIds: *cint', 'boneIds: *[0]cint')
+    apply_unbounded_array(i, 'boneWeights: *float32', 'boneWeights: *[0]float32')
+    apply_unbounded_array(i, 'vboId: *cuint', 'vboId: *[0]cuint')
   end
 
-  if string.find(final_result[i], 'global Shader ') then
-    final_result[i] = string.gsub(final_result[i], 'locs: %*cint', 'locs: *[0]cint')
+  if find(final_result[i], 'global Shader ', 1, true) then
+    apply_unbounded_array(i, 'locs: *cint', 'locs: *[0]cint')
   end
 
-  if string.find(final_result[i], 'global Material ') then
-    final_result[i] = string.gsub(final_result[i], 'maps: %*MaterialMap', 'maps: *[0]MaterialMap')
-    final_result[i] = string.gsub(final_result[i], 'params: %*float32', 'params: *[0]float32')
+  if find(final_result[i], 'global Material ', 1, true) then
+    apply_unbounded_array(i, 'maps: *MaterialMap', 'maps: *[0]MaterialMap')
+    apply_unbounded_array(i, 'params: *float32', 'params: *[0]float32')
   end
 
-  if string.find(final_result[i], 'global Model ') then
-    final_result[i] = string.gsub(final_result[i], 'meshes: %*Mesh', 'meshes: *[0]Mesh')
-    final_result[i] = string.gsub(final_result[i], 'materials: %*Material', 'materials: *[0]Material')
-    final_result[i] = string.gsub(final_result[i], 'meshMaterial: %*cint', 'meshMaterial: *[0]cint')
-    final_result[i] = string.gsub(final_result[i], 'bones: %*BoneInfo', 'bones: *[0]BoneInfo')
-    final_result[i] = string.gsub(final_result[i], 'bindPose: %*Transform', 'bindPose: *[0]Transform')
+  if find(final_result[i], 'global Model ', 1, true) then
+    apply_unbounded_array(i, 'meshes: *Mesh', 'meshes: *[0]Mesh')
+    apply_unbounded_array(i, 'materials: *Material', 'materials: *[0]Material')
+    apply_unbounded_array(i, 'meshMaterial: *cint', 'meshMaterial: *[0]cint')
+    apply_unbounded_array(i, 'bones: *BoneInfo', 'bones: *[0]BoneInfo')
+    apply_unbounded_array(i, 'bindPose: *Transform', 'bindPose: *[0]Transform')
   end
 
-  if string.find(final_result[i], 'global ModelAnimation ') then
-    final_result[i] = string.gsub(final_result[i], 'bones: %*BoneInfo', 'bones: *[0]BoneInfo')
-    final_result[i] = string.gsub(final_result[i], 'framePoses: %*%*Transform', 'framePoses: *[0]*[0]Transform')
+  if find(final_result[i], 'global ModelAnimation ', 1, true) then
+    apply_unbounded_array(i, 'bones: *BoneInfo', 'bones: *[0]BoneInfo')
+    apply_unbounded_array(i, 'framePoses: **Transform', 'framePoses: *[0]*[0]Transform')
   end
+
+  -- apply undbounded array type to functions
+  if find(final_result[i], 'Raylib.LoadFileData(fileName: cstring, bytesRead: *cuint): *cuchar', 1, true) then
+    apply_unbounded_array(i, 'Raylib.LoadFileData(fileName: cstring, bytesRead: *cuint): *cuchar', 'Raylib.LoadFileData(fileName: cstring, bytesRead: *cuint): *[0]cuchar')
+  end
+  if find(final_result[i], 'Raylib.GetDirectoryFiles(dirPath: cstring, count: *cint): *cstring', 1, true) then
+    apply_unbounded_array(i, 'Raylib.GetDirectoryFiles(dirPath: cstring, count: *cint): *cstring', 'Raylib.GetDirectoryFiles(dirPath: cstring, count: *cint): *[0]cstring')
+  end
+  if find(final_result[i], 'Raylib.GetDroppedFiles(count: *cint): *cstring', 1, true) then
+    apply_unbounded_array(i, 'Raylib.GetDroppedFiles(count: *cint): *cstring', 'Raylib.GetDroppedFiles(count: *cint): *[0]cstring')
+  end
+  if find(final_result[i], 'Raylib.CompressData(data: *cuchar, dataLength: cint, compDataLength: *cint): *cuchar', 1, true) then
+    apply_unbounded_array(i, 'Raylib.CompressData(data: *cuchar, dataLength: cint, compDataLength: *cint): *cuchar', 'Raylib.CompressData(data: *[0]cuchar, dataLength: cint, compDataLength: *cint): *[0]cuchar')
+  end
+  if find(final_result[i], 'Raylib.DecompressData(compData: *cuchar, compDataLength: cint, dataLength: *cint): *cuchar', 1, true) then
+    apply_unbounded_array(i, 'Raylib.DecompressData(compData: *cuchar, compDataLength: cint, dataLength: *cint): *cuchar', 'Raylib.DecompressData(compData: *[0]cuchar, compDataLength: cint, dataLength: *cint): *[0]cuchar')
+  end
+  if find(final_result[i], 'Raylib.DrawLineStrip(points: *Vector2, numPoints: cint, color: Color): void', 1, true) then
+    apply_unbounded_array(i, 'Raylib.DrawLineStrip(points: *Vector2, numPoints: cint, color: Color): void', 'Raylib.DrawLineStrip(points: *[0]Vector2, numPoints: cint, color: Color): void')
+  end
+  if find(final_result[i], 'Raylib.LoadImageEx(pixels: *Color, width: cint, height: cint): Image', 1, true) then
+    apply_unbounded_array(i, 'Raylib.LoadImageEx(pixels: *Color, width: cint, height: cint): Image', 'Raylib.LoadImageEx(pixels: *[0]Color, width: cint, height: cint): Image')
+  end
+  if find(final_result[i], 'Raylib.GetImageData(image: Image): *Color', 1, true) then
+    apply_unbounded_array(i, 'Raylib.GetImageData(image: Image): *Color', 'Raylib.GetImageData(image: Image): *[0]Color')
+  end
+  if find(final_result[i], 'Raylib.GetImageDataNormalized(image: Image): *Vector4', 1, true) then
+    apply_unbounded_array(i, 'Raylib.GetImageDataNormalized(image: Image): *Vector4', 'Raylib.GetImageDataNormalized(image: Image): *[0]Vector4')
+  end
+  if find(final_result[i], 'Raylib.ImageExtractPalette(image: Image, maxPaletteSize: cint, extractCount: *cint): *Color', 1, true) then
+    apply_unbounded_array(i, 'Raylib.ImageExtractPalette(image: Image, maxPaletteSize: cint, extractCount: *cint): *Color', 'Raylib.ImageExtractPalette(image: Image, maxPaletteSize: cint, extractCount: *cint): *[0]Color')
+  end
+  if find(final_result[i], 'Raylib.LoadFontEx(fileName: cstring, fontSize: cint, fontChars: *cint, charsCount: cint): Font', 1, true) then
+    apply_unbounded_array(i, 'Raylib.LoadFontEx(fileName: cstring, fontSize: cint, fontChars: *cint, charsCount: cint): Font', 'Raylib.LoadFontEx(fileName: cstring, fontSize: cint, fontChars: *[0]cint, charsCount: cint): Font')
+  end
+  if find(final_result[i], 'Raylib.LoadFontData(fileName: cstring, fontSize: cint, fontChars: *cint, charsCount: cint, type: cint): *CharInfo', 1, true) then
+    apply_unbounded_array(i, 'Raylib.LoadFontData(fileName: cstring, fontSize: cint, fontChars: *cint, charsCount: cint, type: cint): *CharInfo', 'Raylib.LoadFontData(fileName: cstring, fontSize: cint, fontChars: *[0]cint, charsCount: cint, type: cint): *[0]CharInfo')
+  end
+  if find(final_result[i], 'Raylib.GenImageFontAtlas(chars: *CharInfo, recs: **Rectangle, charsCount: cint, fontSize: cint, padding: cint, packMethod: cint): Image', 1, true) then
+    apply_unbounded_array(i, 'Raylib.GenImageFontAtlas(chars: *CharInfo, recs: **Rectangle, charsCount: cint, fontSize: cint, padding: cint, packMethod: cint): Image', 'Raylib.GenImageFontAtlas(chars: *[0]CharInfo, recs: **[0]Rectangle, charsCount: cint, fontSize: cint, padding: cint, packMethod: cint): Image')
+  end
+  if find(final_result[i], 'Raylib.TextJoin(textList: *cstring, count: cint, delimiter: cstring): cstring', 1, true) then
+    apply_unbounded_array(i, 'Raylib.TextJoin(textList: *cstring, count: cint, delimiter: cstring): cstring', 'Raylib.TextJoin(textList: *[0]cstring, count: cint, delimiter: cstring): cstring')
+  end
+  if find(final_result[i], 'Raylib.TextSplit(text: cstring, delimiter: cchar, count: *cint): *cstring', 1, true) then
+    apply_unbounded_array(i, 'Raylib.TextSplit(text: cstring, delimiter: cchar, count: *cint): *cstring', 'Raylib.TextSplit(text: cstring, delimiter: cchar, count: *cint): *[0]cstring')
+  end
+  if find(final_result[i], 'Raylib.TextToUtf8(codepoints: *cint, length: cint): cstring', 1, true) then
+    apply_unbounded_array(i, 'Raylib.TextToUtf8(codepoints: *cint, length: cint): cstring', 'Raylib.TextToUtf8(codepoints: *[0]cint, length: cint): cstring')
+  end
+  if find(final_result[i], 'Raylib.GetCodepoints(text: cstring, count: *cint): *cint', 1, true) then
+    apply_unbounded_array(i, 'Raylib.GetCodepoints(text: cstring, count: *cint): *cint', 'Raylib.GetCodepoints(text: cstring, count: *cint): *[0]cint')
+  end
+  if find(final_result[i], 'Raylib.LoadMeshes(fileName: cstring, meshCount: *cint): *Mesh', 1, true) then
+    apply_unbounded_array(i, 'Raylib.LoadMeshes(fileName: cstring, meshCount: *cint): *Mesh', 'Raylib.LoadMeshes(fileName: cstring, meshCount: *cint): *[0]Mesh')
+  end
+  if find(final_result[i], 'Raylib.LoadMaterials(fileName: cstring, materialCount: *cint): *Material', 1, true) then
+    apply_unbounded_array(i, 'Raylib.LoadMaterials(fileName: cstring, materialCount: *cint): *Material', 'Raylib.LoadMaterials(fileName: cstring, materialCount: *cint): *[0]Material')
+  end
+  if find(final_result[i], 'Raylib.LoadModelAnimations(fileName: cstring, animsCount: *cint): *ModelAnimation', 1, true) then
+    apply_unbounded_array(i, 'Raylib.LoadModelAnimations(fileName: cstring, animsCount: *cint): *ModelAnimation', 'Raylib.LoadModelAnimations(fileName: cstring, animsCount: *cint): *[0]ModelAnimation')
+  end
+  if find(final_result[i], 'Raylib.GetWaveData(wave: Wave): *float32', 1, true) then
+    apply_unbounded_array(i, 'Raylib.GetWaveData(wave: Wave): *float32', 'Raylib.GetWaveData(wave: Wave): *[0]float32')
+  end
+
+  end
+
 end
 
 table.insert(final_result, [[
